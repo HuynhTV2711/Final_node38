02ec4dbbf144374b5a4ed119d3f4bd67c169e1ca
HUYNHTV2711 <huynhptit2711@gmail.com>
2024-02-26 23:37:29 +0700

    Tạo các resource API trong dự án

---
+++ src/type-of-job/type-of-job.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { TypeOfJobService } from './type-of-job.service';
+import { CreateTypeOfJobDto } from './dto/create-type-of-job.dto';
+import { UpdateTypeOfJobDto } from './dto/update-type-of-job.dto';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('type-of-job')
+@Controller('type-of-job')
+export class TypeOfJobController {
+  constructor(private readonly typeOfJobService: TypeOfJobService) {}
+
+  @Post()
+  create(@Body() createTypeOfJobDto: CreateTypeOfJobDto) {
+    return this.typeOfJobService.create(createTypeOfJobDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.typeOfJobService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.typeOfJobService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTypeOfJobDto: UpdateTypeOfJobDto) {
+    return this.typeOfJobService.update(+id, updateTypeOfJobDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.typeOfJobService.remove(+id);
+  }
+}
