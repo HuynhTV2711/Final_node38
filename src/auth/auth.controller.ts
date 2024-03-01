343d2af20f7dc7f19eeb97cda8fff6a3de1af315
HoangAnh <hoanganh51199@gmail.com>
2024-03-02 00:37:09 +0700

    fix ten swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -24,13 +24,5 @@ export class AuthController {
     return this.authService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
-  }
+  
 }
