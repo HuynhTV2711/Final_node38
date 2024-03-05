0aea80db6a20e5a623a6246b836e3b89a3ff1c0c
HUYNHTV2711 <huynhptit2711@gmail.com>
2024-03-05 23:04:12 +0700

    create, update hire job

--- src/hire-job/hire-job.controller.ts
+++ src/hire-job/hire-job.controller.ts
@@ -6,20 +6,29 @@ import {
   Patch,
   Param,
   Delete,
+  Put,
 } from '@nestjs/common';
 import { HireJobService } from './hire-job.service';
 import { CreateHireJobDto } from './dto/create-hire-job.dto';
 import { UpdateHireJobDto } from './dto/update-hire-job.dto';
-import { ApiParam, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiParam, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('ThueCongViec')
 @Controller('/api/thue-cong-viec')
 export class HireJobController {
   constructor(private readonly hireJobService: HireJobService) {}
 
-  @Post()
-  create(@Body() createHireJobDto: CreateHireJobDto) {
+  @ApiBody({ type: CreateHireJobDto })
+  @Post('/create-hire-job')
+  create(@Body() createHireJobDto: CreateHireJobDto): Promise<any> {
     return this.hireJobService.create(createHireJobDto);
+  }
+
+  @ApiBody({ type: UpdateHireJobDto })
+  @ApiParam({ name: 'id', required: true, description: 'id' })
+  @Put('/update-hire-job/:id')
+  update(@Body() body: UpdateHireJobDto, @Param('id') id: string) {
+    return this.hireJobService.update(body, +id);
   }
 
   @Get()
