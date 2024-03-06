ff0bffd429c112c93a86757c728b7a4e9d1b7e56
HUYNHTV2711 <huynhptit2711@gmail.com>
2024-03-06 22:01:06 +0700

    Auth các API

--- src/hire-job/hire-job.controller.ts
+++ src/hire-job/hire-job.controller.ts
@@ -7,23 +7,30 @@ import {
   Param,
   Delete,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { HireJobService } from './hire-job.service';
 import { CreateHireJobDto } from './dto/create-hire-job.dto';
 import { UpdateHireJobDto } from './dto/update-hire-job.dto';
-import { ApiBody, ApiParam, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiParam, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { RoleGuards } from 'src/strategy/role.stratey';
 
 @ApiTags('ThueCongViec')
 @Controller('/api/thue-cong-viec')
 export class HireJobController {
   constructor(private readonly hireJobService: HireJobService) {}
 
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
   @ApiBody({ type: CreateHireJobDto })
   @Post('/create-hire-job')
   create(@Body() createHireJobDto: CreateHireJobDto): Promise<any> {
     return this.hireJobService.create(createHireJobDto);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
   @ApiBody({ type: UpdateHireJobDto })
   @ApiParam({ name: 'id', required: true, description: 'id' })
   @Put('/update-hire-job/:id')
@@ -31,11 +38,15 @@ export class HireJobController {
     return this.hireJobService.update(body, +id);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   findAll() {
     return this.hireJobService.findAll();
   }
 
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
   @ApiParam({ name: 'page', required: true, description: 'page number' })
   @ApiParam({ name: 'size', required: true, description: 'page size' })
   @Get('/:page/:size')
@@ -46,12 +57,17 @@ export class HireJobController {
     return this.hireJobService.hireJobPagination(skip, numSize);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
   @ApiParam({ name: 'id', required: true, description: 'id' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.hireJobService.findOne(+id);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(RoleGuards)
+  @UseGuards(AuthGuard('jwt'))
   @ApiParam({ name: 'id', required: true, description: 'id' })
   @Delete(':id')
   remove(@Param('id') id: string) {
