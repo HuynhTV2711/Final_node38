2831c74f21c51ec9ca7706456315b5819d0a3713
HUYNHTV2711 <huynhptit2711@gmail.com>
2024-03-06 22:22:57 +0700

    validate login, signup và create user

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,8 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export default class loginDTO {
   @ApiProperty({ type: String, description: 'Email', required: true })
+  @IsEmail()
   email: string;
   @ApiProperty({ type: String, description: 'Password', required: true })
+  @IsNotEmpty()
   pass_word: string;
 }
